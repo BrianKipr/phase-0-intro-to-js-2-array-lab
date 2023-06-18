a08e73262780480075a247c9f2998b82467d94d8
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-18 16:46:26 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats[1])
+
+function array(beforeEach){
+  const cats = ('Milo', 'Otis', 'Garfield');
+  cats.length = 0;
+  cats;
+}
+
+const destructivelyAppendCat = ["Milo", "Otis", "Garfield"]
+destructivelyAppendCat.push(Ralp);
+destructivelyAppendCat;
+
+function AppendCat(cats){
+  const cat = ["Milo", "Otis", "Garfield", "Ralph"]
+  cat.push(Ralph);
+  cat;
+}
+function arrayName = ("Bob", "Milo", "Otis", "Garfield");
+const lastElement = arrayName.pop();
+arrayName;
+
+
